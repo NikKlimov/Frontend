23bff7489683dcc2cb26093a5b524cda972cabe3
Vladislav Tkachenko <webkadiz@gmail.com>
2024-02-25 16:40:45 +0300

    add service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />)
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err))
+  })
+}
